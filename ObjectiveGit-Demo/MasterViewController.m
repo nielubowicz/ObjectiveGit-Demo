e379dd00a5286253599d658ec04f247631e776c1
Chris Nielubowicz <cnielubowicz@mobiquityinc.com>
2015-11-01 21:24:21 -0500

    Adds fast-forward, rewind and restore buttons to move forward, backward, and reset to head

--- ObjectiveGit-Demo/MasterViewController.m
+++ ObjectiveGit-Demo/MasterViewController.m
@@ -14,27 +14,31 @@
 
 @interface MasterViewController () <UITextViewDelegate>
 
+@property (strong, nonatomic) UIView *headerView;
 @property (strong, nonatomic) UITextView *committedText;
-
+@property (assign, nonatomic) NSInteger commitIndex;
 @property NSMutableArray<GTCommit *> *objects;
 
 @end
 
 @implementation MasterViewController
 
+- (void)dealloc {
+    [self removeObserver:self forKeyPath:@"commitIndex"];
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
+    self.commitIndex = 0;
+    [self addObserver:self forKeyPath:@"commitIndex" options:NSKeyValueObservingOptionNew context:nil];
+    
     // Do any additional setup after loading the view, typically from a nib.
     self.navigationItem.leftBarButtonItem = self.editButtonItem;
-
-    UIBarButtonItem *addButton = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemAdd target:self action:@selector(insertNewObject:)];
-    self.navigationItem.rightBarButtonItem = addButton;
     self.detailViewController = (DetailViewController *)[[self.splitViewController.viewControllers lastObject] topViewController];
 }
 
 - (void)viewWillAppear:(BOOL)animated {
     self.clearsSelectionOnViewWillAppear = self.splitViewController.isCollapsed;
-    NSLog([[GitHistoryManager sharedInstance] loadCurrentData]);
     [super viewWillAppear:animated];
 }
 
@@ -42,8 +46,30 @@
     [super viewDidAppear:animated];
     self.objects = [[[GitHistoryManager sharedInstance] commitHistory] mutableCopy];
     [self.committedText setText:[[GitHistoryManager sharedInstance] loadCurrentData]];
-    self.tableView.tableHeaderView = self.committedText;
+    self.tableView.tableHeaderView = self.headerView;
     [self.tableView reloadData];
+}
+
+- (UIView *)headerView {
+    if (_headerView == nil) {
+        CGSize tableViewSize = self.tableView.bounds.size;
+        _headerView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, tableViewSize.width, 132)];
+        [_headerView addSubview:self.committedText];
+        
+        UIToolbar *toolbar = [[UIToolbar alloc] initWithFrame:CGRectMake(0, 88, tableViewSize.width, 44)];
+        NSArray<UIBarButtonItem *> *items = @[
+                                              [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemRewind
+                                                                                            target:self action:@selector(moveToPreviousCommit)],
+                                              [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemFastForward
+                                                                                            target:self action:@selector(moveToNextCommit)],
+                                              [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemRefresh
+                                                                                            target:self action:@selector(restoreToHead)]
+                                              ];
+        [toolbar setItems:items];
+        [_headerView addSubview:toolbar];
+    }
+
+    return _headerView;
 }
 
 - (UITextView *)committedText {
@@ -78,6 +104,35 @@
         controller.navigationItem.leftBarButtonItem = self.splitViewController.displayModeButtonItem;
         controller.navigationItem.leftItemsSupplementBackButton = YES;
     }
+}
+
+
+#pragma mark - Toolbar methods
+
+- (void)moveToNextCommit {
+    if (self.commitIndex <= 0) {
+        return;
+    }
+    
+    self.commitIndex--;
+}
+
+- (void)moveToPreviousCommit {
+    if (self.commitIndex >= self.objects.count) {
+        return;
+    }
+    
+    self.commitIndex++;
+}
+
+- (void)restoreToHead {
+    self.commitIndex = 0;
+}
+
+- (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary<NSString *,id> *)change context:(void *)context {
+    GTCommit *commit = self.objects[self.commitIndex];
+    NSString *dataString = [[GitHistoryManager sharedInstance] loadDataFromCommit:commit];
+    [self.committedText setText:dataString];
 }
 
 #pragma mark - Table View
